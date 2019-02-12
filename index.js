d30966bf3b6cc79f4ff2ae61ec3375adeb745813
sowiecki <nase00@gmail.com>
2019-02-12 00:42:37 -0600

    Readd all hashes.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = new Koa();
 const router = new Router();
 
 // Requests can't be sent with #'s in them
-const rehashHexCodes = params => {
+const readdHashes = params => {
   const rehashedParams = Object.assign({}, params, {
     theme_color: `#${params.theme_color}`,
     background_color: `#${params.background_color}`
@@ -21,14 +21,14 @@ const rehashHexCodes = params => {
     delete rehashedParams.background_color;
   }
 
-  return rehashedParams;
+  return rehashedParams.replace(/HASH/g, "#");
 };
 
 router.get("/manifest.json", (ctx, next) => {
   const rawParams = ctx.originalUrl.replace("/manifest.json", "");
   const params = queryString.parse(rawParams);
 
-  ctx.body = rehashHexCodes(JSON.parse(params.content));
+  ctx.body = readdHashes(JSON.parse(params.content));
 });
 
 server.use(router.routes());
