f286cbc42d6b48d6ed241bc27f168d256abcea9a
sowiecki <nase00@gmail.com>
2019-02-12 00:30:56 -0600

    Better.

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const PORT = process.env.PORT || 3000;
 const server = new Koa();
 const router = new Router();
 
-router.get("/manifest.json", async (ctx, next) => {
-  const params = queryString.parse(
-    ctx.originalUrl.replace("/manifest.json", "")
-  );
-  console.log("foo", ctx.originalUrl);
-  ctx.body = params.content;
+// Requests can't be sent with #'s in them
+const rehashHexCodes = params => {
+  const rehashedParams = Object.assign({}, params, {
+    theme_color: `#${params.theme_color}`,
+    background_color: `#${params.background_color}`
+  });
+
+  if (!params.theme_color) {
+    delete rehashedParams.theme_color;
+  }
+
+  if (!params.background_color) {
+    delete rehashedParams.background_color;
+  }
+
+  return rehashedParams;
+};
+
+router.get("/manifest.json", (ctx, next) => {
+  const rawParams = ctx.originalUrl.replace("/manifest.json", "");
+  const params = queryString.parse(rawParams);
+
+  ctx.body = rehashHexCodes(JSON.parse(params.content));
 });
 
 server.use(router.routes());
